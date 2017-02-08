7cc7219cacaafec6db3789b9edd7948b0c38e31e
Nico Taljaard <nico.taljaard@authgate.tech>
2017-02-08 11:57:34 +0200

    Adding Pages

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
   <head>
-    <meta charset="utf-8">
-    <title>Login</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+    <title>TEST</title>    
   </head>
   <body>
     <h1> Hell ya!!! </h1>
